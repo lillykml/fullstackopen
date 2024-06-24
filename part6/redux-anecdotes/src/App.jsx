325fe0b00a65083efa0cdd3b9504059c4a9e6413
lillykml <64362067+lillykml@users.noreply.github.com>
2024-06-24 12:42:03 +0200

    Re-factored Anecdote Form

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote, addAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,14 +9,6 @@ const App = () => {
   const vote = (id) => {
     dispatch(voteAnecdote(id))
   }
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(addAnecdote(content))
-  }
-
 
   return (
     <div>
@@ -32,10 +25,7 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name='anecdote' /></div>
-        <button type='submit'>create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,25 @@
+import { addAnecdote } from "../reducers/anecdoteReducer"
+import { useDispatch } from "react-redux"
+
+const AnecdoteForm = () => {
+
+    const dispatch = useDispatch()
+
+    const add = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        dispatch(addAnecdote(content))
+    }
+
+    return(
+        <div>
+            <form onSubmit={add}>
+                <div><input name='anecdote' /></div>
+                <button type='submit'>create</button>
+            </form>
+        </div>
+    )
+}
+
+export default AnecdoteForm
