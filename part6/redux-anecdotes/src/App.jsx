8beda9c152f45b1c8336b3c0159bdaefa625611a
lillykml <64362067+lillykml@users.noreply.github.com>
2024-06-24 12:28:27 +0200

    Implemented voting functionality

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -5,7 +5,12 @@ const App = () => {
   const dispatch = useDispatch()
 
   const vote = (id) => {
-    console.log('vote', id)
+    dispatch({
+      type: 'VOTE',
+      payload: {
+        id: id
+      }
+    })
   }
 
   return (
